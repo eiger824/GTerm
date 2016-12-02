7fe5113d6716fffb0e92d17c6fe68e237202c3fc
eiger824 <santiago.pagola@alten.se>
2016-12-02 13:23:29 +0100

    prompt works & next and prev items with ctrl

--- include/gui.hpp
+++ include/gui.hpp
@@ -11,25 +11,29 @@
 #include <QStringList>
 
 namespace gterm {
-	class GTerm : public QWidget {
-		Q_OBJECT
-	public:
-		GTerm(QWidget* parent=0);
-		~GTerm();
-	protected:
-		void keyPressEvent(QKeyEvent* event);
-	private slots:
-		void textChangedSlot();
-	private:
-		std::string exec(const char* cmd);
-		void moveCursor();
-	private:
-		QTextEdit* m_command_line;
-		QVBoxLayout* m_main_layout;
-		QString m_token;
-		QTextEdit* m_output;
-		QStringList m_history;
-		unsigned m_history_index;
-	};
+  class GTerm : public QWidget {
+    Q_OBJECT
+  public:
+    GTerm(QWidget* parent=0);
+    ~GTerm();
+  protected:
+    void keyPressEvent(QKeyEvent* event);
+    void keyReleaseEvent(QKeyEvent* event);
+  private slots:
+    void textChangedSlot();
+  private:
+    std::string exec(const char* cmd);
+    void moveCursor();
+    void resetPrompt();
+  private:
+    QTextEdit* m_command_line;
+    QVBoxLayout* m_main_layout;
+    QString m_token;
+    QTextEdit* m_output;
+    QStringList m_history;
+    unsigned m_history_index;
+    QString m_pwd;
+    bool m_ctrl;
+  };
 }
 #endif

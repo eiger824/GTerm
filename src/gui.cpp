2db61d0c592d49b220a71ae5156de456c4d6665a
eiger824 <santiago.pagola@alten.se>
2016-12-02 14:06:57 +0100

    implemented local clear function

--- src/gui.cpp
+++ src/gui.cpp
@@ -19,12 +19,12 @@ namespace gterm {
     m_output->setStyleSheet("border: 2px solid black;");
     m_output->setAlignment(Qt::AlignLeft);
     m_output->clearFocus();
-    m_pwd = "[" + exec("pwd").remove("\n") + "]" + m_token;
-    m_command_line = new QTextEdit(m_pwd);
-    moveCursor();
+    m_command_line = new QTextEdit();
+    resetPrompt();
     connect(m_command_line, SIGNAL(textChanged()), this, SLOT(textChangedSlot()));
     m_main_layout->addWidget(m_output);
     m_main_layout->addWidget(m_command_line);
+    setTabOrder(m_command_line, m_output);
     setLayout(m_main_layout);
     show();
   }
@@ -44,13 +44,15 @@ namespace gterm {
       if (m_history_index < m_history.size()-1 && !m_history.isEmpty()) {
 	++m_history_index;
 	m_command_line->setText(m_pwd + m_history.at(m_history_index));
-      }	
+      }
     } else if (event->key() == 66 && m_ctrl) { //'b', backward
       if (m_history_index > 0 && !m_history.isEmpty()) {
 	--m_history_index;
 	m_command_line->setText(m_pwd + m_history.at(m_history_index));
       }
     } else if (event->key() == 16777249) m_ctrl = true;
+
+    moveCursor();
   }
 
   void GTerm::keyReleaseEvent(QKeyEvent* event) {
@@ -66,9 +68,14 @@ namespace gterm {
       std::cout << "Command to send: [" << command.toStdString() << "]\n";
       m_history << command;
       m_history_index = m_history.size() -1;
+      //check if cd
       if (!command.contains("cd", Qt::CaseSensitive)) {
+	if (command.contains("clear", Qt::CaseSensitive)) {
+	  m_output->clear();
+	} else {
 	  m_output->append(exec(command.toStdString().c_str())
 			   + "\t\t@@@@@@@@@@@@@@@@@@@@@@\t\t");
+	}
       } else {
 	int code = chdir(command.mid(3).toStdString().c_str());
 	m_output->append("PWD now is " + command.mid(3)
@@ -83,7 +90,7 @@ namespace gterm {
   QString GTerm::exec(const char* cmd) {
     char command[200];
     strcpy(command,cmd);
-    strcat(command," 2>&1");
+    strcat(command," 2>&1"); //to redirect stderr to stdout
     FILE* pipe = popen(command, "r");
     if (!pipe) return "ERROR";
     char buffer[128];
